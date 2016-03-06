adbb1a0d33a740ed348590f5f43366b7d21ddbf6
Justin Hileman <justin@justinhileman.info>
2016-03-06 12:25:26 -0800

    Use `init.fish` instead of `*.load` to require files on load.

--- init.fish
+++ init.fish
@@ -1,5 +1,5 @@
 for gvm_path in (find $HOME/.gvm/*/current/bin -maxdepth 0)
-  _prepend_path $gvm_path
+  set PATH $gvm_path $PATH
   set -l module_info (echo $gvm_path | perl -ne 'm|(.*/.gvm/(\w+)/current)/bin| && print "$1 $2"')
   set -l module_dir (echo $module_info | awk -F' ' '{print $1}')
   set -l module_name (echo $module_info | awk -F' ' '{print $2}')
@@ -27,4 +27,3 @@ end
 if test -z $JAVA_HOME
   echo "GVM: JAVA_HOME not set please set JAVA_HOME."
 end
-
